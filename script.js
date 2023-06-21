800ba618cc2e218015e86c75bc6f36cab7efb71a
AdilTehranli <adiljt@code.edu.az>
2023-06-21 16:31:03 +0400

    Updated template

--- script.js
+++ script.js
@@ -11,9 +11,21 @@ form.addEventListener('submit', async (e)=>{
     try{
 
         let response = await fetch(`https://source.unsplash.com/random/900×700/?${input.value}`)
-        input.value=""
+        
         image.src=response.url
     }catch(err){
-        
+            console.log(err);
     }
+    
+    let api_key='3a2071049e76ca09f49d543a419959fa';
+    try{
+        let response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${api_key}`)
+       let result= await response.json();
+        console.log(result);
+
+    }catch(err){
+        console.log(err);
+    }
+
+
 })
